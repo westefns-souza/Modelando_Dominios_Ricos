a91aa34ee1e0118f9290928631b13ba442ce7cae
Westefns Damião de Souza Silva <l4304@liga.org.br>
2020-02-03 14:49:03 -0300

    Corrupção de código

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var subscription = new Subscription(null);
+            var student =  new Student("Westefns", "Souza", "121.250.434-83", "westefns@outlook.com.br");
+            student.AddSubscription(subscription);
+        }
+    }
+}
